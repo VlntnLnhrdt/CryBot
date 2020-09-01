1407c9e32e9421b39719166b58fdeeb30b6fdbb3
Valentin <valentin.linhardt@gmail.com>
2020-09-01 10:38:47 +0200

    - adding timestamp and comments

--- src/io/Logger.java
+++ src/io/Logger.java
@@ -1,13 +1,25 @@
 package io;
 
+import java.util.Date;
+
 public class Logger {
+
+    // TODO Schreiben der Logs in eine Datei
+
+    // Beschreibung:
+    // Zuerst muss logStart aufgerufen werden, sobald der Task fertig ist, muss logEnd aufgerufen werden
+    // Mehrere logStart hintereinander ohne logEnd führen zu Anzeigefehlern
+
+    private static Date TIMESTAMP;
 
     public static void logStart(String text) {
         System.out.print("Status: "+text+" ...");
+        TIMESTAMP = new Date();
     }
 
     public static void logEnd() {
-        System.out.println(" 100%");
+        long ms = new Date().getTime() - TIMESTAMP.getTime();
+        System.out.println(" 100% ("+ms+"ms)");
     }
 
 }
